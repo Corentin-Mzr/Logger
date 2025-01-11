b63058c68ad320bf0bdf1f6ef1e6abfd52093bcf
Corentin M <DeadStorm19@gmail.com>
2025-01-11 21:11:32 +0100

    Build folder

---
+++ build/CMakeFiles/LogWriter.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LogWriter.dir/src/log_writter.cpp.obj"
+  "CMakeFiles/LogWriter.dir/src/log_writter.cpp.obj.d"
+  "CMakeFiles/LogWriter.dir/src/main.cpp.obj"
+  "CMakeFiles/LogWriter.dir/src/main.cpp.obj.d"
+  "LogWriter.exe"
+  "LogWriter.exe.manifest"
+  "LogWriter.pdb"
+  "libLogWriter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LogWriter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/LogWriter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LogWriter.
+# This may be replaced when dependencies are built.
